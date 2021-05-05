c145048effb39e7d3d383465383ff23085fd1680
Muhammad Eryawan <mochyawn@gmail.com>
2021-05-05 14:59:29 +0700

    buat constructor

--- wpu1.php
+++ wpu1.php
@@ -13,6 +13,15 @@ class Produk
         $penerbit = "ga ada",
         $harga = 0;
 
+    //constructor
+    public function __construct($judul, $penulis, $penerbit, $harga)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
     public function cetakLabel()
     {
         return "$this->penulis, $this->penerbit";
@@ -20,18 +29,9 @@ class Produk
 };
 
 //buat objek yg lengkap
-$produk3 = new Produk();
-$produk3->judul = "Naruto";
-$produk3->penulis = "ya penulisnya naruto";
-$produk3->penerbit = "Shonen";
-$produk3->harga = "30000";
-echo $produk3->cetakLabel();
+$produk1 = new Produk("Naruto", "ya Penulisnya Naruto", "Shonen", 30000);
+$produk2 = new Produk("Counter Strike", "ya yg bikin cs", "Valve", 40000);
 
+echo $produk1->cetakLabel();
 echo "<hr>";
-
-$produk4 = new Produk();
-$produk4->judul = "Counter Strike";
-$produk4->penulis = "ya yg bikin cs";
-$produk4->penerbit = "Valve";
-$produk4->harga = 40000;
-echo $produk4->cetakLabel();
+echo $produk2->cetakLabel();
