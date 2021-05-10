604051667be55dd07da110bc8ebdab14bc671db8
Muhammad Eryawan <mochyawn@gmail.com>
2021-05-10 08:24:10 +0700

    staatic keyword terikat dengan kelas

--- static.php
+++ static.php
@@ -5,6 +5,7 @@ class Nama
     public static $numWord = 2;
     public static function buatNama()
     {
+        //self mirip this
         self::$numWord += 2;
         return "Jumlah Nama " . self::$numWord . " Kali <br>";
     }
@@ -20,4 +21,16 @@ echo Nama::buatNama();
 echo Nama::buatNama();
 
 $eryawan = new Nama;
+echo "<hr>";
+echo $eryawan->buatNama();
+echo $eryawan->buatNama();
+echo "<hr>";
+echo Nama::buatNama();
+echo Nama::buatNama();
+echo Nama::buatNama();
+
+echo "<hr>";
+echo "<hr>";
+echo $eryawan->buatNama();
+
 var_dump($eryawan);
