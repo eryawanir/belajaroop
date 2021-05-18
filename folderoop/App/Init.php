adebaf5c5d4067602a45915e98ee09f2d1db33e2
Muhammad Eryawan <mochyawn@gmail.com>
2021-05-18 10:39:22 +0700

    dir path

--- folderoop/App/Init.php
+++ folderoop/App/Init.php
@@ -1,5 +1,5 @@
-<?php 
+<?php
 
-spl_autoload_register(function( $class ){
-    require_once 'Produk/'. $class . '.php';
+spl_autoload_register(function ($class) {
+    require_once __DIR__ . '\Produk\\' . $class . '.php';
 });
